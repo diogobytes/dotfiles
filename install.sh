7737f4b526cf714844f49f71db8b0deec8c24b64
Diogo G <diogofoguedes@hotmail.com>
2024-02-29 15:31:15 +0000

    First Commit

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# ln -sf <target> <symlink>
+# -f option forces the creation of the link. It removes any existing link or file with the same name before creating a new symlink
+#
+#   We first create the directories because if the directory does not exist it will reproduce an error message 
+#####
+#nvim#
+#####
+
+mkdir -p "$HOME/.config/nvim"
+mkdir -p "$HOME/.config/nvim/undo"
+ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
+
+
+#####
+#X11##
+#####
+
+rm -rf "$HOME/.config/X11"
+ln -s "$HOME/dotfiles/X11" "$HOME/.config"
