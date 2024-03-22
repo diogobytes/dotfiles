a0368d2c66d0f5334175452cf5815bf1e32fca26
Diogo <diogo.guedes111@gmail.com>
2024-03-22 22:17:06 +0100

    new update

--- install.sh
+++ install.sh
@@ -1,36 +1,31 @@
 #!/bin/bash
 
-# ln -sf <target> <symlink>
-# -f option forces the creation of the link. It removes any existing link or file with the same name before creating a new symlink
-#
-#   We first create the directories because if the directory does not exist it will reproduce an error message 
-#####
-#nvim#
-#####
+########
+# nvim #
+########
 
 mkdir -p "$HOME/.config/nvim"
 mkdir -p "$HOME/.config/nvim/undo"
+
 ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
-
-
-#####
-#X11##
-#####
 
 rm -rf "$HOME/.config/X11"
 ln -s "$HOME/dotfiles/X11" "$HOME/.config"
 
+######
+# i3 #
+######
 
-#####
-#i3## 
-#####
 rm -rf "$HOME/.config/i3"
 ln -s "$HOME/dotfiles/i3" "$HOME/.config"
 
-#####
-#zsh#
-#####
-#####
+#######
+# Zsh #
+#######
+
 mkdir -p "$HOME/.config/zsh"
 ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
 ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh/aliases"
+rm -rf "$HOME/.config/zsh/external"
+ln -sf "$HOME/dotfiles/zsh/external" "$HOME/.config/zsh"
