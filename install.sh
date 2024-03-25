63b459467525e8c7dd522ea4c7142e0dee3f53cc
Diogo Guedes <45937530+diogobytes@users.noreply.github.com>
2024-03-25 10:00:15 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,16 +30,7 @@ nvim --noplugin +PlugUpdate +qa
 rm -rf "$XDG_CONFIG_HOME/i3"
 ln -s "$DOTFILES/i3" "$XDG_CONFIG_HOME"
 
-#######
-# Zsh #
-#######
 
-mkdir -p "$XDG_CONFIG_HOME/zsh"
-ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
-ln -sf "$DOTFILES/zsh/.zshrc" "$XDG_CONFIG_HOME/zsh"
-ln -sf "$DOTFILES/zsh/aliases" "$XDG_CONFIG_HOME/zsh/aliases"
-rm -rf "$XDG_CONFIG_HOME/zsh/external"
-ln -sf "$DOTFILES/zsh/external" "$XDG_CONFIG_HOME/zsh"
 
 #########
 # Fonts #
